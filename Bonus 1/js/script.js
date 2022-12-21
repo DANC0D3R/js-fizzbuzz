6d8c8a4ceecb2bc707fedb0c7dd6493dcd7060fa
Daniele Minieri <danieleminieri99@gmail.com>
2022-12-21 17:34:44 +0100

    Completato Bonus 1

--- Bonus 1/js/script.js
+++ Bonus 1/js/script.js
@@ -1,19 +1,33 @@
-// Ciclo che si ripete per 100 volte
-for(let i = 0; i <= 99; i++){
-    // Il numero è divisibile sia per 3 che per 5
-    if((i % 3 == 0) && (i % 5 == 0)){
-        console.log('FizzBuzz');
-    } 
+// Assegnazione dell'elemento a una variabile
+const container = document.getElementById('container');
+
+// Ciclo che crea il quadrato con il numero per 100 volte
+for (let i = 0; i <= 99; i++){
+    // Creo il quadrato con il numero
+    const square = document.createElement('div');
+
+    square.className = 'square';
+
+    container.append(square);
+
+    let content = i;
+
+    // Condizioni 
+    // Il numero è divisibile sia per 5 che per 3
+    if ( (i % 3 == 0) && (i % 5 == 0) ){
+        // Testo
+        content = 'FizzBuzz';
+    }
     // Il numero è divisibile per 3
     else if (i % 3 == 0){
-        console.log('Fizz');
+        // Testo
+        content = 'Fizz';
     }
     // Il numero è divisibile per 5
-    else if (i % 5 == 0){
-        console.log('Buzz');
+    else if (i % 5 == 0){ 
+        // Testo
+        content = 'Buzz';
     }
-    // Il numero non è divisibile ne per 3 ne per 5
-    else{
-        console.log(i);
-    }
+
+    square.innerHTML = '<p class="content">' + content + '</p>';
 };
